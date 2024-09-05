bc3c445871c861af62639e106dac3c8be3c80907
Jetmarte <obcollazos@gmail.com>
2024-09-05 15:47:57 -0400

    configuracion completa

---
+++ db/connect.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT, 10) // Asegúrate de convertir el puerto a número
+});
+
+module.exports.connectPostgres = async () => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');  // Ejemplo de consulta fecha actual
+    client.release();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.rows),
+      message: 'Success in connecting to postgres database!'
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: 'Error connecting to the Database!',
+    };
+  }
+};
---
+++ src/controllers/handler/handler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.getHello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Pase a Serverless v1.0! Su función se ejecutó correctamente',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+};
+
+
